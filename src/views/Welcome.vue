1ca3c9b055dbc8548f0636e76c73c9b27d582398
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2021-10-23 15:36:35 +1100

    Different welcome states

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -8,13 +8,19 @@
         width="100%"
       />
     </div>
-    <div v-if="state === 'all'" class="right">
-      <img src="../assets/logo-dark.svg" width="60%" />
-      <div class="green-bg">
-        <button class="long green">Join call</button>
-      </div>
-      <button class="long outline">Sign in</button>
-      <button class="long outline">Create an account</button>
+
+    <!-- TOGGLE ON -->
+    <div v-if="onlyCalls" class="right green-bg">
+        <h2>Join a call</h2>
+        <p>
+          You can join a call using a code from your club, which you may have
+          received by email.
+        </p>
+        <p class="label">Enter group code:</p>
+        <input type="text" />
+        <button class="long green sans">
+          Proceed to call settings
+        </button>
       <div class="bottom-section">
         <p>I only use this for calls, don't ask me to sign in</p>
         <label class="switch">
@@ -28,8 +34,126 @@
       </div>
     </div>
 
-    <div v-else class="right">
+    <!-- ALL OPTIONS -->
+    <div v-else-if="state === 'all'" class="right">
+      <img src="../assets/logo-dark.svg" width="60%" />
+      <div class="green-bg">
+        <button class="long green" @click="state = 'join'">Join call</button>
+      </div>
+      <button class="long outline" @click="state = 'sign'">Sign in</button>
+      <button class="long outline" @click="state = 'create'">
+        Create an account
+      </button>
+      <div class="bottom-section">
+        <p>I only use this for calls, don't ask me to sign in</p>
+        <label class="switch">
+          <input
+            type="checkbox"
+            @click="$store.commit('toggleOnlyCalls')"
+            v-model="onlyCalls"
+          />
+          <span class="slider"></span>
+        </label>
+      </div>
     </div>
+
+    <!-- JOIN A CALL -->
+    <div v-else-if="state === 'join'" class="right">
+      <div class="green-bg col">
+        <h2>Join a call</h2>
+        <i class="fas fa-times close fa-3x" @click="state = 'all'"></i>
+        <p>
+          You can join a call using a code from your club, which you may have
+          received by email.
+        </p>
+        <p class="label">Enter group code:</p>
+        <input type="text" />
+        <button class="long green sans">
+          Proceed to call settings
+        </button>
+      </div>
+      <button class="long outline" @click="state = 'sign'">Sign in</button>
+      <button class="long outline" @click="state = 'create'">
+        Create an account
+      </button>
+      <div class="bottom-section">
+        <p>I only use this for calls, don't ask me to sign in</p>
+        <label class="switch">
+          <input
+            type="checkbox"
+            @click="$store.commit('toggleOnlyCalls')"
+            v-model="onlyCalls"
+          />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </div>
+
+    <!-- SIGN IN -->
+    <div v-else-if="state === 'sign'" class="right">
+      <div class="bordered">
+        <h2>Sign in</h2>
+        <i class="fas fa-times close fa-3x" @click="state = 'all'"></i>
+        <p class="label">Email:</p>
+        <input type="text" />
+        <p class="label">Password:</p>
+        <input type="text" />
+        <button class="long black sans">
+          Continue
+        </button>
+      </div>
+
+      <button class="long green" @click="state = 'join'">Join a call</button>
+      <button class="long outline" @click="state = 'create'">
+        Create an account
+      </button>
+      <div class="bottom-section">
+        <p>I only use this for calls, don't ask me to sign in</p>
+        <label class="switch">
+          <input
+            type="checkbox"
+            @click="$store.commit('toggleOnlyCalls')"
+            v-model="onlyCalls"
+          />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </div>
+
+    <!-- CREATE AN ACCOUNT -->
+    <div v-else-if="state === 'create'" class="right">
+      <div class="bordered">
+        <h2>Create an account</h2>
+        <i class="fas fa-times close fa-3x" @click="state = 'all'"></i>
+        <p class="label">Email:</p>
+        <input type="text" />
+        <p class="label">Password:</p>
+        <input type="text" />
+        <p class="label">Confirm password:</p>
+        <input type="text" />
+        <button class="long black sans">
+          Continue
+        </button>
+      </div>
+
+      <button class="long green" @click="state = 'join'">Join a call</button>
+      <button class="long outline" @click="state = 'sign'">
+        Sign in
+      </button>
+      <div class="bottom-section">
+        <p>I only use this for calls, don't ask me to sign in</p>
+        <label class="switch">
+          <input
+            type="checkbox"
+            @click="$store.commit('toggleOnlyCalls')"
+            v-model="onlyCalls"
+          />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -104,13 +228,12 @@ export default {
     flex-direction: column;
     align-items: center;
 
-    .green-bg {
-      background-color: rgba($green, 0.5);
-      display: flex;
-      justify-content: center;
-      padding: 16px 0;
-      margin: 16px 0;
-      width: 100%;
+    &.green-bg {
+        background-color: rgba($green, 0.5);
+    }
+
+    h2 {
+        margin-block-end: 0;
     }
 
     .long {
@@ -120,11 +243,77 @@ export default {
       @include serif;
       font-size: 24px;
       margin: 16px 0;
+
+      &.sans {
+        @include sans-serif;
+        color: white;
+      }
     }
+
+    .green-bg {
+      background-color: rgba($green, 0.5);
+      display: flex;
+      justify-content: center;
+      padding: 16px 0;
+      margin: 16px 0;
+      width: 100%;
+
+      &.col {
+        flex-direction: column;
+        margin-top: 0;
+        align-items: center;
+
+        p {
+          width: 70%;
+          font-weight: 700;
+
+          &.label {
+            @include serif;
+            color: black;
+            font-size: 24px;
+          }
+        }
+      }
+    }
+
+    .bordered {
+      display: flex;
+      justify-content: center;
+      padding: 16px 0;
+      margin: 16px 0;
+      width: 90%;
+      border-bottom: 2px solid black;
+
+      flex-direction: column;
+      margin-top: 0;
+      align-items: center;
+
+      .long {
+          width: 77.8%;
+      }
+
+      p {
+        width: 70%;
+        font-weight: 700;
+
+        &.label {
+          @include serif;
+          color: black;
+          font-size: 24px;
+        }
+      }
+    }
+
+    
 
     .green {
       color: white;
       border: 2px solid $green;
+    }
+
+    .black {
+      color: white;
+      border: 2px solid black;
     }
 
     .bottom-section {
