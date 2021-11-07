2c0f9638f978b0f872e09617f6cf25d52be370c6
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2021-11-07 22:58:46 +1100

    Add tutorial gifs

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -30,7 +30,10 @@
       <div v-if="stateInd == 1" class="content">
         <h2>Let’s take a look at the Groups Page!</h2>
         <h4>
-          The tabs on the left-hand side can help you navigate back to ‘home’ or switch between your different groups.<br /><br />Here, in your groups page, you can notify your group friends whether you’ll attend the next meeting!<br/><br/>Now let’s check out the message board!
+          The tabs on the left-hand side can help you navigate back to ‘home’ or
+          switch between your different groups.<br /><br />Here, in your groups
+          page, you can notify your group friends whether you’ll attend the next
+          meeting!<br /><br />Now let’s check out the message board!
         </h4>
         <button class="long black" @click="stateInd += 1">Let's go!</button>
       </div>
@@ -38,9 +41,12 @@
       <div v-if="stateInd == 2" class="content">
         <h2>Check out the Message Board!</h2>
         <h4>
-          In your group’s Message Board, you can create your own post by clicking ‘add new post’.<br /><br />
-          Search for other group member’s posts by using the search ar at the top.<br /><br />
-          And last but not least, have fun interacting with your friend’s by replying to their posts!
+          In your group’s Message Board, you can create your own post by
+          clicking ‘add new post’.<br /><br />
+          Search for other group member’s posts by using the search ar at the
+          top.<br /><br />
+          And last but not least, have fun interacting with your friend’s by
+          replying to their posts!
         </h4>
         <button class="long black" @click="stateInd += 1">What's next?</button>
       </div>
@@ -48,8 +54,10 @@
       <div v-if="stateInd == 3" class="content">
         <h2>This is your Contacts Page</h2>
         <h4>
-          Check out how you can communicate with your friends from all of your groups here!<br /><br />
-          You can search for them using the ‘search bar’ or scroll through the cards.
+          Check out how you can communicate with your friends from all of your
+          groups here!<br /><br />
+          You can search for them using the ‘search bar’ or scroll through the
+          cards.
         </h4>
         <button class="long black" @click="stateInd += 1">What's next?</button>
       </div>
@@ -58,14 +66,24 @@
         <h2>Edit your Profile!</h2>
         <h4>
           This is where you can see your profile.<br /><br />
-          Here, you can edit your details by simply clicking in the spaces, change your password and leave your group by clicking ‘edit’ and then ‘quit’.<br/><br/>
-          And that’s it! If you ever have any questions, you can access me by clicking the info icon on the top right corner of every page.
+          Here, you can edit your details by simply clicking in the spaces,
+          change your password and leave your group by clicking ‘edit’ and then
+          ‘quit’.<br /><br />
+          And that’s it! If you ever have any questions, you can access me by
+          clicking the info icon on the top right corner of every page.
         </h4>
-        <button class="long black" @click="$router.push('/home')">Let's start!</button>
+        <button class="long black" @click="$router.push('/home')">
+          Let's start!
+        </button>
       </div>
-
     </div>
-    <div class="right"></div>
+    <div class="right">
+      <img v-if="stateInd == 0" src="../assets/tut1.png" class="side-image" />
+      <img v-if="stateInd == 1" src="../assets/tut2.gif" class="side-image" />
+      <img v-if="stateInd == 2" src="../assets/tut3.gif" class="side-image" />
+      <img v-if="stateInd == 3" src="../assets/tut4.gif" class="side-image" />
+      <img v-if="stateInd == 4" src="../assets/tut5.gif" class="side-image" />
+    </div>
   </div>
 </template>
 
@@ -143,6 +161,13 @@ export default {
   .right {
     width: 50%;
     background: #ebdfcf;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .side-image {
+      width: 90%;
+    }
   }
 }
 </style>
